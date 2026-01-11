3442489b4555c750220db31bc48dfed97f1fe071
BrandonCimino <brandon.cimino@gmail.com>
2026-01-10 21:01:02 -0600

    Added an initializer that doesn't care about the child's name.

--- Scenes/LazyChild.cs
+++ Scenes/LazyChild.cs
@@ -10,17 +10,22 @@ public sealed class LazyChild<T>(Func<Node, T> truancyOfficer)
     where T : Node {
     private T? _child;
 
-    public LazyChild(string uniqueName) : this(parent => parent.EnumerateChildren()
+    /// <summary>
+    /// Assumes that there will be <b>exactly one</b> child - at <b>any depth</b> - of type <typeparamref name="T"/>.
+    /// </summary>
+    public LazyChild() : this(parent => parent.EnumerateChildren()
+        .OfType<T>()
+        .Single()
+    ) { }
+
+    /// <summary>
+    /// Assumes that there will be <b>exactly one</b> child - at <b>any depth</b> - with the given <see cref="Node.Name"/>.
+    /// </summary>
+    /// <param name="uniqueName"></param>
+    public LazyChild(StringName uniqueName) : this(parent => parent.EnumerateChildren()
         .OfType<T>()
         .Single(it => it.Name == uniqueName)
     ) { }
-
-    public static LazyChild<T> ByName(string uniqueName) {
-        return new LazyChild<T>(parent => parent.EnumerateChildren()
-            .OfType<T>()
-            .Single(it => it.Name == uniqueName)
-        );
-    }
 
     public T Get(Node parent) {
         return _child ??= truancyOfficer(parent);
